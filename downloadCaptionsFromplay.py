6c59f2c3154a585b785c9ac97f97b01c947fdab4
dustinjhenderson <dustinjhenderson@gmail.com>
2017-09-07 17:39:18 -0700

    start on srt download
    https://developers.google.com/youtube/v3/docs/captions/download look here. download for api i returning a problem

--- downloadCaptionsFromplay.py
+++ downloadCaptionsFromplay.py
@@ -194,6 +194,25 @@ def getTitle(vidId):
 	#print "\n"
 	return results[start:end]
 	
+def download_caption(youtube, caption_id, tfmt):
+	subtitle = youtube.captions().download( id=caption_id, tfmt=tfmt).execute()
+	
+# Authorize the request and store authorization credentials.
+def get_authenticated_service(args):
+  flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE, scope=YOUTUBE_READ_WRITE_SSL_SCOPE,
+    message=MISSING_CLIENT_SECRETS_MESSAGE)
+
+  storage = Storage("%s-oauth2.json" % sys.argv[0])
+  credentials = storage.get()
+
+  if credentials is None or credentials.invalid:
+    credentials = run_flow(flow, storage, args)
+
+  # Trusted testers can download this discovery document from the developers page
+  # and it should be in the same directory with the code.
+  with open("youtube-v3-api-captions.json", "r") as f:
+    doc = f.read()
+    return build_from_document(doc, http=credentials.authorize(httplib2.Http()))
 	
 '''*********************************** Main Part of Script ***********************************'''
 playListId = ""
@@ -203,6 +222,7 @@ videoId = ""
 counter = 0
 lastToken = False
 
+youtube = get_authenticated_service(args) #added to try and get captions api working
 
 fileLocation = getFileLocation()
 print "test id currently: PLYYARuW-EUbEzCScH6unajic_fkaOG7Yv" #my test playlist
@@ -212,6 +232,8 @@ token = findNextToken(apiResponce)
 print token
 videoId = findVidID(apiResponce)
 print getTitle(videoId)
+
+download_caption(youtube, videoId, 'srt')
 
 while(lastToken == False):
 	apiResponce = str(playlist_items_list_by_playlist_id(service, pageToken=token, part='contentDetails', maxResults=1, playlistId=playListId))
